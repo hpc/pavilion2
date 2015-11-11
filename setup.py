e526c9806df4dfab1a455caa13bf2937e9ca72a5
Craig Idler <cwi@lanl.gov>
2015-11-11 14:11:30 -0700

    verson 1.0.9

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ except ImportError:
 config = {
     'description': 'Pavilion - High Performance Cluster Testing Framework',
     'long_description': 'Software framework and tools for testing and analyzing HPC system health',
-    'author': 'LANL',
-    'url': 'git.lanl.gov/cwi/pavilion',
-    'download_url': 'git@git.lanl.gov/cwi/pavilion',
+    'author': 'DOE',
+    'download_url': 'https://github.com/losalamos/Pavilion',
     'author_email': 'cwi@lanl.gov',
-    'version': '1.0.2',
+    'version': '1.0.9',
     'packages': ['PAV'],
     'packages': ['test'],
     'name': 'pavilion'
