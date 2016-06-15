4eaebd1f4dca9b0309086b74f938c04fcad51ec7
Chris DeJager <dejager@lanl.gov>
2016-06-15 16:29:02 -0600

    made the moabjobcontroller backwards compadible.
    The last commit required that the os and machine type be in the default
    yaml. This breaks old setups with the new version. I have now tried to
    implement safe options for moab (like slurm) so that if an option does
    not exist but is not nessary it still runs.

--- PAV/modules/moabjobcontroller.py
+++ PAV/modules/moabjobcontroller.py
@@ -124,7 +124,7 @@ class MoabJobController(JobController):
         msub_cmd = "msub -V "
 
         # handle optionally specified queue
-        if self.configs['moab']['queue']:
+        if 'queue' in self.configs['moab'] and self.configs['moab']['queue']:
             msub_cmd += "-q " + self.configs['moab']['queue'] + " "
 
         # add test name
@@ -140,23 +140,23 @@ class MoabJobController(JobController):
 
         # get target segment, if specified
         ts = ''
-        if self.configs['moab']['target_seg']:
+        if 'target_seg' in self.configs['moab'] and self.configs['moab']['target_seg']:
             ts = self.configs['moab']['target_seg']
 
         reservation = ''
-        if self.configs['moab']['reservation']:
+        if 'reservation' in self.configs['moab'] and self.configs['moab']['reservation']:
             reservation = self.configs['moab']['reservation']
 
         node_list = ''
-        if self.configs['moab']['node_list']:
+        if 'node_list' in self.configs['moab'] and self.configs['moab']['node_list']:
             node_list = self.configs['moab']['node_list']
 
         machine_type = ''
-        if self.configs['moab']['machine_type']:
+        if 'machine_type' in self.configs['moab'] and self.configs['moab']['machine_type']:
             machine_type = self.configs['moab']['machine_type']
 
         os_type = ''
-        if self.configs['moab']['os']:
+        if 'os' in self.configs['moab'] and self.configs['moab']['os']:
             os_type = self.configs['moab']['os']
 
         # accounting file? or just log it?
