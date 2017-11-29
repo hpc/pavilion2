77fcf7446996a0292d0515a0dcee5024ccf8cc01
Christopher DeJager <dejager@lanl.gov>
2017-11-29 16:09:46 -0700

    Fixed issue #39 and incremented version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'author': 'DOE',
     'download_url': 'https://github.com/losalamos/Pavilion',
     'author_email': 'dejager@lanl.gov',
-    'version': '1.1.14',
+    'version': '1.1.15',
     'packages': ['PAV', 'test'],
     'name': 'pavilion'
 }
