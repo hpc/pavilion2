4e532eb403e67664e9163d79b8878c9e0539da10
sly <sly@lanl.gov>
2018-09-07 13:09:30 -0600

    Added file in which the symlink function actually is.

---
+++ PAV/modules/cp2ws.py
@@ -0,0 +1,89 @@
+#!python
+
+#  ###################################################################
+#
+#  Disclaimer and Notice of Copyright 
+#  ==================================
+#
+#  Copyright (c) 2015, Los Alamos National Security, LLC
+#  All rights reserved.
+#
+#  Copyright 2015. Los Alamos National Security, LLC. 
+#  This software was produced under U.S. Government contract 
+#  DE-AC52-06NA25396 for Los Alamos National Laboratory (LANL), 
+#  which is operated by Los Alamos National Security, LLC for 
+#  the U.S. Department of Energy. The U.S. Government has rights 
+#  to use, reproduce, and distribute this software.  NEITHER 
+#  THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES 
+#  ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY 
+#  FOR THE USE OF THIS SOFTWARE.  If software is modified to 
+#  produce derivative works, such modified software should be 
+#  clearly marked, so as not to confuse it with the version 
+#  available from LANL.
+#
+#  Additionally, redistribution and use in source and binary 
+#  forms, with or without modification, are permitted provided 
+#  that the following conditions are met:
+#  -  Redistributions of source code must retain the 
+#     above copyright notice, this list of conditions 
+#     and the following disclaimer. 
+#  -  Redistributions in binary form must reproduce the 
+#     above copyright notice, this list of conditions 
+#     and the following disclaimer in the documentation 
+#     and/or other materials provided with the distribution. 
+#  -  Neither the name of Los Alamos National Security, LLC, 
+#     Los Alamos National Laboratory, LANL, the U.S. Government, 
+#     nor the names of its contributors may be used to endorse 
+#     or promote products derived from this software without 
+#     specific prior written permission.
+#   
+#  THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC 
+#  AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+#  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+#  IN NO EVENT SHALL LOS ALAMOS NATIONAL SECURITY, LLC OR CONTRIBUTORS 
+#  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+#  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+#  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+#  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+#  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+#  OF SUCH DAMAGE.
+#
+#  ###################################################################
+
+
+import os
+
+def sym2ws(src, dest):
+    """ Function for copying an entire directory and file tree using
+        only symlinks in the appropriate structure.
+
+        Can also symlink a single file into the destination folder.
+    """
+    if not os.path.isdir( dest ):
+        print('Error: Destination path %s is not a directory.' % dest )
+        raise
+
+    if not os.path.isabs( src ):
+        src = os.path.abspath( src )
+
+    if not os.path.isabs( dest ):
+        dest = os.path.abspath( dest )
+
+    if not os.path.isdir( src ):
+        if not os.path.isfile( src ):
+            print('Error: %s is neither a file nor directory.' % src )
+            raise
+        else:
+            os.symlink( src, os.path.join( dest, os.path.basename( src ) ) )
+            return
+
+    for item in os.scandir( src ):
+        if item.is_dir():
+            os.mkdir(os.path.join(dest, item.name))
+            sym2ws(os.path.join(src, item.name), os.path.join(dest, item.name))
+        else:
+            os.symlink( os.path.join(src, item.name),
+                        os.path.join(dest, item.name) )
