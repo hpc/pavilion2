277cc7a69dbd4eda1007ff070b6629d176236417
Paul Ferrell <pferrell@lanl.gov>
2018-10-24 16:51:07 -0600

    Finished the string_parser tests.

--- test/tests/string_parser_tests.py
+++ test/tests/string_parser_tests.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, unicode_literals, division
 
 import unittest
+import traceback
 
 from pavilion import variables
 from pavilion import string_parser
@@ -36,9 +37,9 @@ class TestStringParser(unittest.TestCase):
     var_set_manager.add_var_set('pav', pav_data)
 
     def test_parser(self):
-        """Check string parsing and variable substitution. Permutations are handled at a higher
-        level, so there's really only a few things to check here."""
+        """Check string parsing and variable substitution."""
 
+        # Strings to test (unparsed string, expected result)
         test_strings = [
             # The empty string is valid.
             ('', ''),
@@ -56,21 +57,56 @@ class TestStringParser(unittest.TestCase):
             # Substring substitution as last item.
             ('Hello [{var2}]', 'Hello 012'),
             # Substring substitution with multiple loop vars and a non-loop var.
-            ('Hello [{var2}{sep}{pav.var2}: ] World.', 'Hello 0-p0 0-p1 1-p0 1-p1 2-p0 2-p1 '
-                                                       'World.'),
+            ('Hello [{var2}{sep}{pav.var2}: ] World.',
+             'Hello 0-p0 0-p1 1-p0 1-p1 2-p0 2-p1 World.'),
             # Substring substitution without spaces.
             ('Hello [{var2}]World.', 'Hello 012World.'),
             # Substring substitution with an escaped space.
-            ('Hello [{var2}]\ World.', 'Hello 012 World.'),
+            ('Hello [{var2}:\ ] World.', 'Hello 0: 1: 2:  World.'),
             # Sub-strings with repeated usage
-            ('Hello [{var4.subvar1}-{var4.subvar2}] World.',
+            ('Hello [{var4.subvar1}-{var4.subvar2}: ] World.',
              'Hello subval0_1-subval0_2 subval1_1-subval1_2 World.'),
             # sub-sub strings
-            ('Hello [{var2} [{var4.subvar1}] ]World.',
-             'Hello 0 subval0_1 subval1_1 1 subval0_1 subval1_1 2 subval0_1 subval1_1 World.'),
+            ('Hello [{var2}-[{var4.subvar1}:-]: ] World.',
+             'Hello 0-subval0_1-subval1_1 1-subval0_1-subval1_1 2-subval0_1-subval1_1 World.'),
 
         ]
 
         for test_str, answer_str in test_strings:
             self.assertEqual(string_parser.parse(test_str).resolve(self.var_set_manager),
                              answer_str)
+
+    def test_parser_errors(self):
+        test_strings = [
+            # Missing close bracket on variable reference.
+            ('Hello {bleh World.', string_parser.ScanError),
+            # Bad variable name
+            ('Hello {;;dasd} World.', string_parser.ScanError),
+            # Bad var_set name (raised by VariableSetManager, re-caught in the tokenizer)
+            ('Hello {;.foo.bar} World.', string_parser.ScanError),
+            # Bad sub_var name
+            ('Hello {pav.bar.;-} World.', string_parser.ScanError),
+            # Extra close bracket
+            ('Hello {hello}} World.', string_parser.ScanError),
+            # Strings cannot end with the escape character.for
+            ('Hello \\', string_parser.ScanError),
+            # The 'Unknown scanning error' exception shouldn't be reachable.
+            # Missing close square bracket.
+            ('Hello [foo World', string_parser.ParseError),
+            # The 'Unknown token of type' exception shouldn't be reachable.
+            # Neither should the two RuntimeError's in Substring start and end.
+        ]
+
+        show_errors = False
+
+        for test_str, error in test_strings:
+            self.assertRaises(error,
+                              lambda: string_parser.parse(test_str).resolve(self.var_set_manager))
+
+            if show_errors:
+                try:
+                    string_parser.parse(test_str).resolve(self.var_set_manager)
+                except error:
+                    traceback.print_exc()
+
+
