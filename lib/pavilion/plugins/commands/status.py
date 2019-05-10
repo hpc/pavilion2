bf14f1b857a960407fdb997266d08e261622612f
sly <sly@lanl.gov>
2019-05-10 10:29:44 -0600

    Updated status command and unittest.

--- lib/pavilion/plugins/commands/status.py
+++ lib/pavilion/plugins/commands/status.py
@@ -2,14 +2,14 @@ from pavilion import commands
 from pavilion import pavtest
 from pavilion import status_file
 from pavilion import utils
-from pavilion.suite import Suite
+from pavilion import suite
 import sys
 
 class StatusCommand(commands.Command):
 
     def __init__(self):
         super().__init__('status', 'Check the status of a test, list of tests,'
-                         'or test suite.', short_help="Get status of tests.")
+                         ' or test suite.', short_help="Get status of tests.")
 
     def _setup_arguments(self, parser):
 
@@ -19,8 +19,8 @@ class StatusCommand(commands.Command):
         )
         parser.add_argument(
             'tests', nargs='*', action='store',
-            help='The name(s) of the tests to check.  These may be any mix of'
-                 'test IDs and suite IDs.  If no value is provided, the most'
+            help='The name(s) of the tests to check.  These may be any mix of '
+                 'test IDs and suite IDs.  If no value is provided, the most '
                  'recent suite submitted by this user is checked.'
         )
 
@@ -30,27 +30,40 @@ class StatusCommand(commands.Command):
         test_statuses = []
 
         if not args.tests:
-            pav_suite = Suite.from_id(pav_config, Suite.load_suite_id())
+            try:
+                pav_suite = suite.Suite.from_id(pav_config,
+                    int(suite.Suite.load_suite_id()))
+            except suite.SuiteError as err:
+                print("No test was specified and last suite run by this"
+                      " user cannot be found.\n{}".format(err))
+                return 1
             test_list = pav_suite.tests
         else:
             for test_id in args.tests:
                 if test_id.startswith('s'):
-                    test_list.extend(Suite.from_id(pav_config,
-                                                   test_id[1:]).tests
-                                    )
+                    try:
+                        test_list.extend(suite.Suite.from_id(pav_config,
+                            int(test_id[1:])).tests)
+                    except suite.SuiteError as err:
+                        print("Suite {} could not be found.\n{}".format(
+                            test_id[1:], err))
+                        return 1
                 else:
                     test_list.append(test_id)
 
         for test_id in test_list:
-            status_file = None
-            pav_test = pavtest.PavTest.from_id(pav_config, test_id)
-            status_file = status_file.StatusFile(pav_test.status).current()
+            status_f = None
+            try:
+                pav_test = pavtest.PavTest.from_id(pav_config, int(test_id))
+            except pavtest.PavTestError or pavtest.PavTestNotFoundError as err:
+                print("Test {} could not be opened.\n{}".format(test_id, err))
+            status_f = status_file.StatusFile(pav_test.status.path).current()
 
             test_statuses.append({
                 'test_id': test_id,
-                'state': status_file.state,
-                'time': status_file.when,
-                'note': status_file.note,
+                'state': status_f.state,
+                'time': str(status_f.when),
+                'note': status_f.note,
             })
 
         if args.json:
@@ -60,3 +73,4 @@ class StatusCommand(commands.Command):
             cols = ['test_id', 'state', 'time', 'note']
             utils.draw_table(sys.stdout, {}, cols, test_statuses,
                              title='Test statuses')
+        return 0
--- test/tests/status_cmd_tests.py
+++ test/tests/status_cmd_tests.py
@@ -1,7 +1,11 @@
-from pavilion import plugins
 from pavilion import commands
+from pavilion import plugins
+from pavilion import pavtest
+from pavilion.suite import Suite
+from pavilion.test_config import format
 from pavilion.unittest import PavTestCase
 import argparse
+import subprocess
 
 
 class StatusTests(PavTestCase):
@@ -31,3 +35,86 @@ class StatusTests(PavTestCase):
         self.assertEqual(args.tests[1], 'test9')
         self.assertEqual(args.json, True)
 
+    def test_status_command(self):
+        """Test status command by generating a suite of tests."""
+
+        config1 = format.TestConfigLoader().validate({
+            'scheduler': 'raw',
+            'run': {
+                'env': {
+                    'foo': 'bar',
+                },
+                'cmds': ['echo "I $foo, punks"'],
+            },
+        })
+
+        config1['name'] = 'run_test0'
+
+        config2 = format.TestConfigLoader().validate({
+            'scheduler': 'raw',
+            'run': {
+                'env': {
+                    'too': 'tar',
+                },
+                'cmds': ['echo "I $too, punks"'],
+            },
+        })
+
+        config2['name'] = 'run_test1'
+
+        config3 = format.TestConfigLoader().validate({
+            'scheduler': 'raw',
+            'run': {
+                'env': {
+                    'too': 'tar',
+                },
+                'cmds': ['sleep 10'],
+            },
+        })
+
+        config3['name'] = 'run_test2'
+
+        configs = [config1, config2, config3]
+
+        tests = [pavtest.PavTest(self.pav_cfg, test) for test in configs]
+
+        for test in tests:
+            test.RUN_SILENT_TIMEOUT = 1
+
+        # Make sure this doesn't explode
+        suite = Suite(self.pav_cfg, tests)
+        test_str = " ".join([str(test) for test in suite.tests])
+
+        # Make sure we got all the tests
+        self.assertEqual(len(suite.tests), 3)
+
+        status_cmd = commands.get_command('status')
+
+        # Testing for individual tests with json output
+        for test in suite.tests:
+            parser = argparse.ArgumentParser()
+            status_cmd._setup_arguments(parser)
+            arg_list = ['-j', str(test)]
+            args = parser.parse_args(arg_list)
+            self.assertEqual(status_cmd.run(self.pav_cfg, args),0)
+
+        # Testing for multiple tests with json output
+        parser = argparse.ArgumentParser()
+        status_cmd._setup_arguments(parser)
+        arg_list = ['-j'].extend(test_str.split())
+        args = parser.parse_args(arg_list)
+        self.assertEqual(status_cmd.run(self.pav_cfg, args),0)
+
+        # Testing for individual tests with tabular output
+        for test in suite.tests:
+            parser = argparse.ArgumentParser()
+            status_cmd._setup_arguments(parser)
+            args = parser.parse_args([str(test)])
+            self.assertEqual(status_cmd.run(self.pav_cfg, args),0)
+
+        # Testing for multiple tests with tabular output
+        parser = argparse.ArgumentParser()
+        status_cmd._setup_arguments(parser)
+        arg_list = test_str.split()
+        args = parser.parse_args(arg_list)
+        self.assertEqual(status_cmd.run(self.pav_cfg, args),0)
