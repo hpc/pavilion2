acea6aaa832202e993f8c3895936a1a98bd2ca9d
Paul Ferrell <pferrell@lanl.gov>
2019-06-20 07:50:45 -0600

    Fixed issues with series id.

--- lib/pavilion/plugins/commands/status.py
+++ lib/pavilion/plugins/commands/status.py
@@ -31,9 +31,9 @@ class StatusCommand(commands.Command):
 
         if not args.tests:
             # Get the last series ran by this user.
-            series_id = series.TestSeries.load_user_series_id()
+            series_id = series.TestSeries.load_user_series_id(pav_cfg)
             if series_id is not None:
-                args.tests.append('s{}'.format(series_id))
+                args.tests.append(series_id)
 
         test_list = []
         for test_id in args.tests:
