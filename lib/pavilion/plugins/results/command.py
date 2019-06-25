807d7742857d033a2b2ab70e555b703e0224dbe7
lapid <lapid@lanl.gov>
2019-06-25 15:35:04 -0600

    everything is in check_args

--- lib/pavilion/plugins/results/command.py
+++ lib/pavilion/plugins/results/command.py
@@ -18,22 +18,56 @@ class Command(result_parsers.ResultParser):
             yc.StrElem(
                 'command', required=True,
                 help_text="Command that will be run."
+            ),
+            yc.StrElem(
+                'success', required=True,
+                default='return_value',
+                help_text="needs to be either return_value or output"
+            ),
+            yc.StrElem(
+                'success_value', required=True,
+                default='0',
+                help_text="success value"
             )
         ])
 
         return config_items
 
-    def _check_args(self, command=None):
+    def _check_args(self, command=None, success=None, success_value=None):
 
-        if command == "":
+        if (command == "") or (success == "") or (success_value == ""):
             raise result_parsers.ResultParserError(
-                "command required"
+                "command, success, and success_value are required"
             )
         # runs command
         else:
-            #os.system(command)
-            subprocess.call(command, shell=True)
+            cmd_result = ""
+            # get output
+            if success == "output":
+                try:
+                    cmd_result = subprocess.check_output(command, 
+                        stderr=subprocess.STDOUT,
+                        shell=True).decode("utf-8")
+                    cmd_result = str(cmd_result)
+                    print("out: " + cmd_result)
+                except:
+                    print("output: command did not work")
+            # get return value
+            else:
+                try:
+                    cmd_result = str(subprocess.call(command, shell=True))
+                    print("return value: " + cmd_result)
+                except:
+                    print("return value: command did not work")
+            # compare out/return_value with success value
+            print("success_value: " + success_value)
+            if cmd_result == str(success_value): 
+                print("command success")
+                success_value = "command_successful"
+            else: 
+                print("command unsuccessful")
+                success_value = "command_unsuccessful"
 
-    def __call__(self, test, file, command=None):
+    def __call__(self, test, file, command=None, success=None, success_value=None):
 
-        return command
+        return "success"
