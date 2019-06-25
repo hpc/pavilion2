3c4ee4eb19cc7f1e202c81d203fac62a681ed3f4
lapid <lapid@lanl.gov>
2019-06-25 13:19:21 -0600

    command runs when _check_args is called

---
+++ lib/pavilion/plugins/results/command.py
@@ -0,0 +1,37 @@
+from pavilion import result_parsers
+import yaml_config as yc
+import re
+import os
+
+
+class Command(result_parsers.ResultParser):
+    """Runs a given command."""
+
+    def __init__(self):
+        super().__init__(name='command')
+
+    def get_config_items(self):
+
+        config_items = super().get_config_items()
+        config_items.extend([
+            yc.StrElem(
+                'command', required=True,
+                help_text="Command that will be run."
+            )
+        ])
+
+        return config_items
+
+    def _check_args(self, command=None):
+
+        if command == "":
+            raise result_parsers.ResultParserError(
+                "command required"
+            )
+        # runs command
+        else:
+            os.system(command)
+
+    def __call__(self, test, file, command=None):
+
+        return command
