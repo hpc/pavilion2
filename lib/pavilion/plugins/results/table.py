9896c80fdc8bfd97f7b428c562c6d4cc1e1e8826
lapid <lapid@lanl.gov>
2019-06-26 10:35:14 -0600

    table result parser takes in row and col names

---
+++ lib/pavilion/plugins/results/table.py
@@ -0,0 +1,40 @@
+from pavilion import result_parsers
+import yaml_config as yc
+import re
+
+
+class Table(result_parsers.ResultParser):
+    """Find matches to the given regex in the given file. The matched string
+    or strings are returned as the result."""
+
+    def __init__(self):
+        super().__init__(name='table')
+
+    def get_config_items(self):
+
+        config_items = super().get_config_items()
+        config_items.extend([
+            yc.ListElem(
+                'row_names', sub_elem=yc.StrElem(),
+                help_text="Column names"
+            ),
+            yc.ListElem(
+                'col_names', sub_elem=yc.StrElem(),
+                help_text="Column names")
+        ])
+
+        return config_items
+
+    def _check_args(self, row_names=None, col_names=None):
+
+        if (not row_names) or (not col_names):
+            raise result_parsers.ResultParserError(
+                "row AND column names required"
+            )
+        else:
+            print("rows: " + str(row_names))
+            print("columns: " + str(col_names))
+
+    def __call__(self, test, file, row_names=None, col_names=None):
+
+        return " "
