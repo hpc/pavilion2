e3a00e42ef22311e016f59aab4776e82a8feb2b7
sly <sly@lanl.gov>
2019-06-28 14:51:34 -0600

    Updating for new results parser format.

--- lib/pavilion/plugins/results/match_count.py
+++ lib/pavilion/plugins/results/match_count.py
@@ -7,8 +7,27 @@ class MatchCount(result_parsers.ResultParser):
 
     PASS = result_parsers.PASS
     FAIL = result_parsers.FAIL
+    ERROR = result_parsers.ERROR
+
+    ACTION_STORE = result_parsers.ACTION_STORE
+    ACTION_TRUE = result_parsers.ACTION_TRUE
+    ACTION_FALSE = result_parsers.ACTION_FALSE
+    ACTION_COUNT = result_parsers.ACTION_COUNT
+
+    PER_FIRST = result_parsers.PER_FIRST
+    PER_LAST = result_parsers.PER_LAST
+    PER_FULLNAME = result_parsers.PER_FULLNAME
+    PER_NAME = result_parsers.PER_NAME
+    PER_LIST = result_parsers.PER_LIST
+    PER_ANY = result_parsers.PER_ANY
+    PER_ALL = result_parsers.PER_ALL
+
+    MATCH_FIRST = result_parsers.MATCH_FIRST
+    MATCH_LAST = result_parsers.MATCH_LAST
+    MATCH_ALL = result_parsers.MATCH_ALL
 
     def __init__(self):
+        # Using the default open_mode of 'r' and default priority.
         super().__init__(name='match_count')
 
     def get_config_items(self):
@@ -20,6 +39,7 @@ class MatchCount(result_parsers.ResultParser):
                 help_text="A word that will be searched for in the output of "
                           "the test that will determine success or failure."
             ),
+            result_parsers.MATCHES_ELEM,
             yc.StrElem(
                 'threshold', default=None,
                 help_text="If a threshold is defined, 'pass' will be returned "
@@ -32,7 +52,7 @@ class MatchCount(result_parsers.ResultParser):
 
         return config_items
 
-    def check_args(self, test, file=None, search=None, threshold=None):
+    def check_args(self, file=None, search=None, threshold=None):
 
         if threshold is not None:
             try:
@@ -50,17 +70,15 @@ class MatchCount(result_parsers.ResultParser):
 
     def __call__(self, test, file=None, search=None, threshold=None):
 
-        total = 0
+        if file is None:
+            file = []
 
-        try:
-            with open(file, "r") as infile:
-                for line in infile.readlines():
-                    total += line.count(search)
-        except (IOError, OSError) as err:
-            raise result_parsers.ResultParserError(
-                "Match result parser could not read input file '{}': {}"
-                .format(file, err)
-            )
+        res_dict = {}
+
+        for res in file:
+            total = 0
+            for line in res.readlines():
+                total += line.count(search)
 
         if threshold is None:
             return total
