5d4ddb881e54aea4c5655bdd5345847bf73c2505
sly <sly@lanl.gov>
2019-07-10 14:10:09 -0600

    Updated for style again.

--- lib/pavilion/plugins/commands/result.py
+++ lib/pavilion/plugins/commands/result.py
@@ -49,9 +49,9 @@ class ResultsCommand(commands.Command):
             try:
                 tests.append(PavTest.load(pav_cfg, id_))
             except PavTestError as err:
-                self.logger.warning("Could not load test % \n %s", id_, err)
+                self.logger.warning("Could not load test %s - %s", id_, err)
             except PavTestNotFoundError as err:
-                self.logger.warning("Could not find test % \n %s", id_, err)
+                self.logger.warning("Could not find test %s - %s", id_, err)
 
         results = []
         for test in tests:
@@ -107,7 +107,7 @@ class ResultsCommand(commands.Command):
                             int(test_id[1:])).tests)
                 except series.TestSeriesError as err:
                     self.logger.warning(
-                        "Suite % could not be found.\n%s", test_id[1:], err
+                        "Suite %s could not be found.\n%s", test_id[1:], err
                     )
                     continue
             else:
