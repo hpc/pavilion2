65ae35d30a768fe9b326c560629e1f32e3554e5c
lapid <lapid@lanl.gov>
2019-07-22 16:17:36 -0600

    prints entire output if any

--- test/tests/extraneous_prints_tests.py
+++ test/tests/extraneous_prints_tests.py
@@ -20,6 +20,7 @@ class ExtraPrintsTest(PavTestCase):
         cmd = "grep -R -I '[^fp]print(' ../lib/pavilion/ --exclude=unittest.py --exclude=utils.py"
         try:
             output = subprocess.check_output(cmd, shell=True)
+            self.maxDiff = None
             self.assertEqual(output.decode("utf-8"),'')
         except subprocess.CalledProcessError as e:
             pass
@@ -28,6 +29,7 @@ class ExtraPrintsTest(PavTestCase):
         cmd = "grep -R -i -I '[^fp]print(' . --exclude=extraneous_prints_tests.py"
         try:
             output = subprocess.check_output(cmd, shell=True)
+            self.maxDiff = None
             self.assertEqual(output.decode("utf-8"),'')
         except subprocess.CalledProcessError as e:
             pass
