89d7a9f9c2229ac089a49aa542a5799f44cf7dbd
lapid <lapid@lanl.gov>
2019-08-06 14:57:20 -0600

    Revert "fixed naming"
    This reverts commit 1b231c62fcdb976c73c27ab0c154e2b13fadf113.

--- lib/pavilion/pav_test.py
+++ lib/pavilion/pav_test.py
@@ -36,8 +36,8 @@ def get_latest_tests(pav_cfg, limit):
     """
 
     test_dir_dict = {}
-    top_dir = Path(pav_cfg.working_dir/'tests')
-    for child in top_dir.iterdir():
+    p = Path(pav_cfg.working_dir/'tests')
+    for child in p.iterdir():
         mtime = os.stat(str(child)).st_mtime
         test_dir_dict[int(str(child.stem))] = mtime
 
