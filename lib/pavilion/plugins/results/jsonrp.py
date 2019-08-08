c562f99b8c75daace28d3ba0c24e7e9d0c67cc58
Kody Jacob Everson <kjeverson@fg-fey1.lanl.gov>
2019-08-08 14:13:12 -0600

    Implemented a regex search to find line before the json.

--- lib/pavilion/plugins/results/jsonrp.py
+++ lib/pavilion/plugins/results/jsonrp.py
@@ -5,41 +5,63 @@ import re
 import json
 
 class Jsonrp(result_parsers.ResultParser):
-    """Converts JSON output into dictionary."""
+    """Converts JSON output into dictionary/list and places it in the
+    RESULTS.json file at the given key."""
 
     def __init__(self):
         super().__init__(
             name='jsonrp',
-            description="Converts JSON output into dictionary.")
+            description="Converts JSON output into dictionary/list and places "
+            "it in the RESULTS.json file at the given key.")
 
     def get_config_items(self):
 
         config_items = super().get_config_items()
+        config_items.extend([
+            yc.StrElem(
+                'regex', default="",
+                help_text="The regex to use to find the line before the JSON "
+                "block in the given file. If nothing is provided it is assumed "
+                "the JSON block is the first thing in the file. "
+            )
+        ])
 
         return config_items
 
-    def _check_args(self, const=None):
+    def _check_args(self, regex=None):
 
-        return
-
-    def __call__(self, test, file):
-
-        # Effectively reads contents of file into single string
-        contents = file.read()
-
-        # Pulls the first JSON block from the file
         try:
-            json_block = re.search(r'{(.|\s)*}', contents)
-            json_block = json_block.group(0)
+            re.compile(regex)
+        except (ValueError, sre_constants.error) as err:
+            raise result_parsers.ResultParserError(
+                "Invalid regular expression: {}".format(err))
 
-        except AttributeError as err:
-            return {}
+        type in ['list', 'dictionary']
 
-        # Loads the JSON as a dictionary
-        try:
-            json_dict = json.loads(json_block)
+    def __call__(self, test, file, regex=None):
 
-        except ValueError as err:
-            return {}
+        decoder = json.JSONDecoder()
 
-        return json_dict
+        if regex:
+            regex = re.compile(regex)
+            found = False
+            rest_of_file = ""
+            for line in file.readlines():
+                if not found and regex.search(line):
+                    found = True
+                elif found:
+                    rest_of_file = rest_of_file + line
+
+            try:
+                result, index = decoder.raw_decode(rest_of_file)
+            except:
+                raise result_parsers.ResultParserError("Could not parse json.")
+
+        else:
+            try:
+                result, index = decoder.raw_decode(file.read())
+            except:
+                raise result_parsers.ResultParserError("Could not parse json.")
+
+        return result
+
