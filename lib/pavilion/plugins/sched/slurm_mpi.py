8830c087af1cd2dd492ce0c6d14426ae3c81e1c9
lapid <lapid@lanl.gov>
2019-09-23 10:29:31 -0600

    added SlurmMPIVars, only test_cmd is overwritten

--- lib/pavilion/plugins/sched/slurm_mpi.py
+++ lib/pavilion/plugins/sched/slurm_mpi.py
@@ -1,25 +1,14 @@
-from pavilion import scriptcomposer
 from pavilion.schedulers import SchedulerPlugin
 from pavilion.schedulers import SchedulerPluginError
 from pavilion.schedulers import SchedulerVariables
 from pavilion.schedulers import dfr_var_method
-from pavilion.var_dict import var_method
-from pavilion.status_file import STATES, StatusInfo
-import os
-import yaml_config as yc
-import re
-import subprocess
-from pathlib import Path
-
 import pavilion.plugins.sched.slurm as slurm
 
 
 class SlurmMPIVars(slurm.SlurmVars):
-
     @dfr_var_method
     def test_cmd(self):
-        """Same variables as Slurm scheduler plugin, only it uses
-        mpirun and not srun"""
+        """Overrides test_cmd in SlurmVars to use mpirun instead of srun"""
         return 'mpirun'
 
 
@@ -31,7 +20,7 @@ class SlurmMPI(slurm.Slurm):
         SchedulerPlugin.__init__(
             self,
             'slurm_mpi',
-            'mpirun',
+            'Schedules tests via Slurm but runs them using mpirun',
             priority=10
         )
 
