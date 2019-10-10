dc1ca4a13d48da50e828a8342f245a297ee56b23
Paul Ferrell <51765748+Paul-Ferrell@users.noreply.github.com>
2019-10-10 16:59:55 -0600

    Fixed issues with slurm num_nodes. (#30)

--- test/tests/extraneous_prints_tests.py
+++ test/tests/extraneous_prints_tests.py
@@ -1,8 +1,8 @@
-from pavilion import commands
 from pavilion import plugins
 from pavilion.unittest import PavTestCase
-import argparse
+import re
 import subprocess
+
 
 class ExtraPrintsTest(PavTestCase):
 
@@ -12,24 +12,41 @@ class ExtraPrintsTest(PavTestCase):
     def tearDown(self):
         plugins._reset_plugins()
 
+    IGNORE_RE = re.compile(r'ext[-_]print:\s*ignore')
+
     def test_for_extra_prints(self):
         """greps for unnecessary dbg_print statements."""
-        
+
         # looks for unnecessary dbg_prints in lib/pavilion directory
-        cmd = "grep -R -I '[^fp]print(' ../lib/pavilion/ --exclude=unittest.py --exclude=utils.py"
+        cmd = "grep -R -I '[^f]print(' ../lib/pavilion/ " \
+              "--exclude=unittest.py --exclude=utils.py"
         try:
-            output = subprocess.check_output(cmd, shell=True)
+            output = subprocess.check_output(cmd, shell=True).decode('utf8')
+            # Filter out lines with a comment saying they're ok.
+            print(output)
+            output = [o for o in output.split('\n') if
+                      o and self.IGNORE_RE.search(o) is None]
+            print(output)
             self.maxDiff = None
-            self.assertEqual(output.decode("utf-8"),'')
+            self.assertEqual(output, [])
         except subprocess.CalledProcessError as e:
             pass
 
+        tests_root = self.PAV_ROOT_DIR/'test'/'tests'
+
         # looks for unnecessary dbg_prints in test directory
-        cmd = "grep -R -i -I '[^fp]print(' . "
-        excludes = "--exclude=extraneous_prints_tests.py --exclude=run_tests --exclude=blarg.py --exclude=poof.py"
+        cmd = ["grep", "-R", "-i", "-I", "[^f]print("]
+        excludes = [
+            'extraneous_prints_tests.py',
+            'blarg.py',
+            'poof.py']
+        cmd.extend(['--exclude={}'.format(excl) for excl in excludes])
+        cmd.append(str(tests_root))
+        print(' '.join(cmd))
+
         try:
-            output = subprocess.check_output(cmd+excludes, shell=True)
+            output = subprocess.check_output(cmd)
             self.maxDiff = None
-            self.assertEqual(output.decode("utf-8"),'')
+            self.assertEqual(output.decode("utf-8"), '')
         except subprocess.CalledProcessError as e:
             pass
