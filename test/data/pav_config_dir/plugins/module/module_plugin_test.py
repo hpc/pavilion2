0993fd99da92e4b43253a27c26f9f68495e918d4
Paul Ferrell <pferrell@lanl.gov>
2019-10-23 15:18:31 -0600

    Added a test to make sure module wrappers actually work.

---
+++ test/data/pav_config_dir/plugins/module/module_plugin_test.py
@@ -0,0 +1,14 @@
+from pavilion import module_wrapper
+from pavilion.module_actions import ModuleLoad
+
+
+class ModuleWrapperTest(module_wrapper.ModuleWrapper):
+
+    def __init__(self):
+        super().__init__('itsa', "")
+
+    def load(self, sys_info, requested_version=None):
+
+        del sys_info
+
+        return [ModuleLoad('fake')], {'FAKE_VAR': "Itsa_real"}
