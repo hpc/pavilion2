1381403fcc0657caf9f8d0fdb230bb334475e066
calvindseamons <calvindseamons@lanl.gov>
2019-11-01 13:16:26 -0600

    fixed lookup of null objects with len()

--- lib/pavilion/test_config/setup.py
+++ lib/pavilion/test_config/setup.py
@@ -538,24 +538,22 @@ def resolve_permutations(raw_test_cfg, pav_vars, sys_vars):
     for var_man in var_men:
         var_man.resolve_references(string_parser.parse)
 
-    #if 'only_if' in raw_test_cfg and raw_test_cfg['only_if']:
-    #    cond_choice(raw_test_cfg['only_if'],'only_if',base_var_man)
-    #if 'not_if' in raw_test_cfg and raw_test_cfg['not_if']:
-    #    cond_choice(raw_test_cfg['not_if'],'not_if',base_var_man)
 
-    if 'only_if' or 'not_if' in raw_test_cfg:
+    if len(raw_test_cfg['only_if']) != 0 or len(raw_test_cfg['not_if']) != 0:
         cond(raw_test_cfg,base_var_man)
 
     return test_cfg, var_men
 
 def cond(config,base_var_man):
-    if 'not_if' in config and 'only_if' not in config:
+    if len(config['not_if']) > 0 and len(config['only_if']) == 0:
+        dbg_print("not")
         if get_match_not_if(config['not_if'],base_var_man):
            dbg_print("Dont make Test")
         else:
             dbg_print("Make Test")
             pass
-    if 'not_if' in config and 'only_if' in config:
+    if len(config['not_if']) > 0 and len(config['only_if']) > 0:
+        dbg_print("only & not")
         if get_match_not_if(config['not_if'],base_var_man) is False:
             if get_match_only_if(config['only_if'],base_var_man):
                 dbg_print("Make Test")
@@ -564,7 +562,8 @@ def cond(config,base_var_man):
                 dbg_print("Dont make Test")
         else:
             dbg_print("Dont make Test")
-    if 'only_if' in config and 'not_if' not in config:
+    if len(config['only_if']) > 0 and len(config['not_if']) == 0:
+        dbg_print("only")
         if get_match_only_if(config['only_if'],base_var_man):
             dbg_print("Make Test")
             pass
