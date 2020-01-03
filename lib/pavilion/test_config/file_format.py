efe7ccf3c961e21b5924cb9c49e5d400c2e475cc
lapid <lapid@lanl.gov>
2020-01-03 10:48:52 -0700

    added test_name and sets

--- lib/pavilion/test_config/file_format.py
+++ lib/pavilion/test_config/file_format.py
@@ -125,8 +125,9 @@ class SeriesConfigLoader(yc.YamlConfigLoader):
 
     ELEMENTS = [
         yc.CategoryElem(
-            'series', sub_elem=yc.KeyedElem(
+            'sets', sub_elem=yc.KeyedElem(
                 elements=[
+                    yc.ListElem('test_names', sub_elem=yc.StrElem()),
                     yc.ListElem('modes', sub_elem=yc.StrElem()),
                     yc.CategoryElem(
                         'only_if', sub_elem=yc.ListElem(sub_elem=yc.StrElem())
