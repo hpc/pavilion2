ac65b7125c1c54196139d8ab163dc9a585605491
Francine Lapid <lapid@lanl.gov>
2020-03-31 13:58:33 -0600

    added rank_by and bind_to config elems to slurm-mpi

--- lib/pavilion/plugins/sched/slurm_mpi.py
+++ lib/pavilion/plugins/sched/slurm_mpi.py
@@ -1,3 +1,4 @@
+import yaml_config as yc
 from pavilion.schedulers import SchedulerPlugin
 from pavilion.schedulers import SchedulerPluginError
 from pavilion.schedulers import SchedulerVariables
@@ -13,8 +14,14 @@ class SlurmMPIVars(slurm.SlurmVars):
 
     @dfr_var_method
     def test_cmd(self):
-        """Overrides test_cmd in SlurmVars to use mpirun instead of srun"""
-        cmd = ['mpirun', '-np', self.procs_per_node()]
+        """Overrides test_cmd in SlurmVars to use mpirun instead of srun."""
+        cmd = ['mpirun', '--map-by ppr:{}:node'.format(self.procs_per_node())]
+
+        if self.sched_config.get('rank_by'):
+            cmd.extend(['--rank-by', self.sched_config.get('rank_by')])
+        if self.sched_config.get('bind_to'):
+            cmd.extend(['--bind-to', self.sched_config.get('bind_to')])
+
         return ' '.join(cmd)
 
 
@@ -28,4 +35,24 @@ class SlurmMPI(slurm.Slurm):
             'slurm_mpi',
             'Schedules tests via Slurm but runs them using mpirun',
             priority=10
+        )
+
+    def get_conf(self):
+        """Add necessary MPI attributes to those of Slurm."""
+        elems = slurm.Slurm.get_slurm_elems(self)
+        elems.extend([
+            yc.StrElem(
+                'rank_by',
+                help_text="Value for `--rank-by`. Default is slot."
+            ),
+            yc.StrElem(
+                'bind_to',
+                help_text="Value for `--bind-to`. Default is core."
+            )
+        ])
+
+        return yc.KeyedElem(
+            self.name,
+            help_text="Configuration for the Slurm scheduler using mpirun.",
+            elements=elems
         )
