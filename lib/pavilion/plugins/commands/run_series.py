0f5b425753844d7906bb655202ed30ed9d21825f
Francine Lapid <lapid@lanl.gov>
2020-04-13 13:13:26 -0600

    travis style error...

--- lib/pavilion/plugins/commands/run_series.py
+++ lib/pavilion/plugins/commands/run_series.py
@@ -26,32 +26,32 @@ class RunSeries(commands.Command):
         pass
 
         # series_name = args.series
-        # 
+
         # series_config_loader = SeriesConfigLoader()
 
         # pylint: disable=W0212
         # series_path = TestConfigResolver._find_config(pav_cfg, 'series',
         #                                               series_name)
-        #
+
         # with series_path.open() as series_file:
         #     series_cfg = series_config_loader.load(series_file)
-        #
+
         #     run_cmd = commands.get_command('run')
         #     arg_parser = arguments.get_parser()
-        #
+
         #     series_obj = series.TestSeries(pav_cfg)
-        #
+
         #     get universal modes
             # universal_modes = series_cfg['modes']
-            #
+
             # set up series
             # sets = series_cfg['series']
             # for set_name, set_info in sets.items():
-            #
+
             #     get all appropriate modes
                 # set_modes = set_info['modes']
                 # all_modes = universal_modes + set_modes
-                #
+
                 # create arguments
                 # pylint: disable=W0212
                 # args_list = ['run', '--series-id {}'.format(series_obj._id)]
@@ -59,8 +59,8 @@ class RunSeries(commands.Command):
                 #     args_list.append('-m{}'.format(mode))
                 # args_list.extend(set_info['test_names'])
                 # args = arg_parser.parse_args(args_list)
-                #
+
                 # call run command to run tests
                 # run_cmd.run(pav_cfg, args)
-        #
+
         # return 0
