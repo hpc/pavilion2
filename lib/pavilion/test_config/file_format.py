e73c0d86583cc8d281eb8b2ca9621a328a93006f
Francine Lapid <lapid@lanl.gov>
2020-04-13 15:53:17 -0600

    added docstring at the end

--- lib/pavilion/test_config/file_format.py
+++ lib/pavilion/test_config/file_format.py
@@ -121,6 +121,35 @@ class EnvCatElem(yc.CategoryElem):
     type = OrderedDict
 
 
+class SeriesConfigLoader(yc.YamlConfigLoader):
+    """This class describes a series file."""
+
+    ELEMENTS = [
+        yc.CategoryElem(
+            'series', sub_elem=yc.KeyedElem(
+                elements=[
+                    yc.ListElem('test_names', sub_elem=yc.StrElem()),
+                    yc.ListElem('modes', sub_elem=yc.StrElem()),
+                    yc.CategoryElem(
+                        'only_if', sub_elem=yc.ListElem(sub_elem=yc.StrElem())
+                    ),
+                    yc.CategoryElem(
+                        'not_if', sub_elem=yc.ListElem(sub_elem=yc.StrElem())
+                    )
+                ]
+            ),
+        ),
+        yc.ListElem(
+            'modes', sub_elem=yc.StrElem()
+        ),
+        yc.ListElem(
+            'on_complete', sub_elem=yc.StrElem()
+        ),
+    ]
+
+    """:cvar ELEMENTS describes elements in Series Config Loader."""
+
+
 class TestConfigLoader(yc.YamlConfigLoader):
     """This class describes a test section in a Pavilion config file. It is
 expected to be added to by various plugins.
