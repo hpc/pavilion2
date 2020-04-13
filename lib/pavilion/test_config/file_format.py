1e98a4b00636ad1bc37554cfc4adee4f3cdd40b5
Francine Lapid <lapid@lanl.gov>
2020-04-13 14:13:11 -0600

    fixing travis/doc errors

--- lib/pavilion/test_config/file_format.py
+++ lib/pavilion/test_config/file_format.py
@@ -124,7 +124,7 @@ class EnvCatElem(yc.CategoryElem):
 class SeriesConfigLoader(yc.YamlConfigLoader):
     """This class describes a series file.
 
-:py:attr:`file_format.TestConfigLoader.ELEMENTS`
+:ref:`pavilion.test_config.file_format.TestConfigLoader`
 """
 
     ELEMENTS = [
