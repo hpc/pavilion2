103a4904ef660c88bb6bfa5c0299db68d03b3bba
Jordan Ogas <jogas@lanl.gov>
2020-05-13 23:12:08 -0600

    guard against runtime file creation outside build context

--- lib/pavilion/test_run.py
+++ lib/pavilion/test_run.py
@@ -393,6 +393,16 @@ class TestRun:
         # Save our newly updated variables.
         self.var_man.save(self._variables_path)
 
+        # Don't allow a file to be written outside of the build context dir.
+        files_to_create = self.config['run']['create_files']
+        if files_to_create:
+            for file, contents in files_to_create.items():
+                file_path = Path(utils.resolve_path(self.build_path / file))
+                if not utils.dir_contains(file_path, self.build_path):
+                    raise TestRunError("'create_file: {}': file path"
+                                       " outside build context."
+                                       .format(file_path))
+
         if not self.skipped:
             self.skipped = self._get_skipped()
 
