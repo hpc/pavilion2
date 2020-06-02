64100f13a13d327827d58c5026636ee3119674b6
Francine Lapid <lapid@lanl.gov>
2020-06-02 13:20:24 -0600

    travis errs

--- test/tests/series_tests.py
+++ test/tests/series_tests.py
@@ -17,16 +17,9 @@ class SeriesFileTests(PavTestCase):
     def test_series_file(self):
         """Test if series works as intended."""
 
-        output.fprint("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", color=output.MAGENTA)
-
         series_cmd = commands.get_command('_series')
         arg_parser = arguments.get_parser()
         series_args = arg_parser.parse_args(['_series', 'series_test'])
 
         series_man = series_cmd.make_series_man(self.pav_cfg, series_args)
-        output.dbg_print(series_man.test_info, color=output.RED)
-        # for test_name, info in series_man.test_info.items():
-        #     for test in info['obj']:
-        #         output.dbg_print(test.results['result'], '\n')
 
-        output.fprint("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", color=output.MAGENTA)
