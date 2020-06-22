c9a04ca8f52c7a532f1a8aea324d92b3ae0e4472
kjeverson <kody.everson@trojans.dsu.edu>
2020-06-22 11:59:21 -0600

    Added command arguments.

--- lib/pavilion/plugins/commands/graph.py
+++ lib/pavilion/plugins/commands/graph.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
 from pavilion import commands
 
@@ -11,6 +13,43 @@ class GraphCommand(commands.Command):
             short_help="Produce graph for tests."
         )
 
+    def _setup_arguments(self, parser):
+
+        parser.add_argument(
+            '--exclude', nargs='*',
+            help='Excludes tests and tests series, must provide '
+                 'specific IDs.'
+        ),
+        parser.add_argument(
+            '--test_name', action='store', default=False,
+            help='Filter tests to graph by test_name.'
+        ),
+        parser.add_argument(
+            '--sys_name', action='store', default=False,
+            help='Filter tests by sys_name.'
+        ),
+        parser.add_argument(
+            '--user', action='store', default=False,
+            help='Filter tests by user.'
+        ),
+        parser.add_argument(
+            '--date', action='store', default=False,
+            help='Filter tests by date.'
+        ),
+        parser.add_argument(
+            'tests', nargs='*', action='store',
+            help='The names of the tests to graph'
+        ),
+        parser.add_argument(
+            'values', nargs='+', action='store',
+            help='Specify the value(s) graphed from the results '
+                 'for each test.'
+        ),
+        parser.add_argument(
+            'Xaxis', nargs=1, action='store',
+            help='Specify the X axis.'
+        )
     def run(self, pav_cfg, args):
 
-        print("GRAPHING....")
+        print(args)
+
