f6a8bf9956da648eda047101d4bd447120c6848f
kjeverson <kody.everson@trojans.dsu.edu>
2020-07-01 11:07:06 -0600

    Added initial test filtration.

--- lib/pavilion/plugins/commands/graph.py
+++ lib/pavilion/plugins/commands/graph.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from datetime import datetime
 
 from pavilion import commands
 from pavilion import series
@@ -20,8 +21,8 @@ class GraphCommand(commands.Command):
 
         parser.add_argument(
             '--exclude', nargs='*',
-            help='Excludes tests and tests series, must provide '
-                 'specific IDs.'
+            help='Exclude tests, series, sys_names, test_names, '
+                 'or users by providing specific IDs or names.'
         ),
         parser.add_argument(
             '--test_name', action='store', default=False,
@@ -62,6 +63,33 @@ class GraphCommand(commands.Command):
         )
     def run(self, pav_cfg, args):
 
-        print(args)
-        pass
+        tests_dir = pav_cfg.working_dir / 'test_runs'
+
+        # No tests provided, check filters, append tests.
+        if not args.tests:
+            for test_path in tests_dir.iterdir():
+                if not test_path.is_dir():
+                    continue
+                # Filter excluded test ids.
+                if test_path.name in args.exclude:
+                   continue
+                # Filter tests by date.
+                if args.date:
+                    pass
+                # Filter tests by user.
+                if test_path.owner() in args.exclude:
+                    continue
+                if args.user and test_path.owner() not in args.user:
+                    continue
+                args.tests.append(test_path.name)
+
+        test_list = []
+        for test_id in args.tests:
+            # Expand series provide, as long as it wasn't meant to be excluded.
+            if test_id.startswith('s') and test_id not in args.exclude:
+                test_list.extend(series.TestSeries.from_id(pav_cfg, test_id))
+            else:
+                test_list.append(int(test_id))
+
+
 
