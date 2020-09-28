c74524f66b1c5ad0f81afdf2f524e6f7c9d18657
kjeverson <kody.everson@trojans.dsu.edu>
2020-09-28 14:13:18 -0600

    Using new test filtering stuff.

--- lib/pavilion/plugins/commands/graph.py
+++ lib/pavilion/plugins/commands/graph.py
@@ -1,9 +1,10 @@
 import errno
 from datetime import datetime
 
+from pavilion import cmd_utils
 from pavilion import commands
+from pavilion import filters
 from pavilion import output
-from pavilion import series
 from pavilion.commands import Command, CommandError
 from pavilion.result import evaluations
 from pavilion.result.base import ResultError
@@ -22,36 +23,13 @@ class GraphCommand(commands.Command):
 
     def _setup_arguments(self, parser):
 
-        parser.add_argument(
-            '--exclude', nargs='*', default=[],
-            help='Exclude tests, series, sys_names, test_names, '
-                 'test suites, or users by providing specific IDs '
-                 'or names.'
-        ),
-        parser.add_argument(
-            '--test_name', action='store', default=False,
-            help='Filter tests by test_name.'
-        ),
-        parser.add_argument(
-            '--suite', action='store', default=False,
-            help='Filter tests by test suite.'
-        ),
-        parser.add_argument(
-            '--sys_name', nargs='+', action='store', default=False,
-            help='Filter tests by sys_name.'
-        ),
-        parser.add_argument(
-            '--user', nargs='+', action='store', default=False,
-            help='Filter tests by user.'
-        ),
-        parser.add_argument(
-            '--date', action='store', default=False,
-            help='Filter tests by date.'
-        ),
+        filters.add_test_filter_args(parser)
+
         parser.add_argument(
             'tests', nargs='*', action='store',
-            help='The names of the tests to graph'
-        ),
+            help='Specific Test Ids to graph.'
+        )
+
         parser.add_argument(
             '--y', nargs='+', action='store',
             help='Specify the value(s) graphed from the results '
@@ -72,13 +50,13 @@ class GraphCommand(commands.Command):
         parser.add_argument(
             '--title', action='store', default="",
             help='Specify the title of the graph.'
-        )
+        ),
 
     def run(self, pav_cfg, args):
 
         try:
             import matplotlib.pyplot as plt
-        except ImportError as err:
+        except ImportError:
             output.fprint("matplotlib not found.", color=output.RED)
             return errno.EINVAL
 
@@ -86,23 +64,13 @@ class GraphCommand(commands.Command):
         try:
             self.validate_args(args)
         except (CommandError, ValueError) as err:
-            output.fprint("Invalid command arguments: \n{}".format(err), color=output.RED)
+            output.fprint("Invalid command arguments: \n{}".format(err),
+                          color=output.RED)
             return errno.EINVAL
 
-        # Expand series, convert test_ids (if provided).
-        try:
-            args.tests = self.normalize_args_tests(pav_cfg, args.tests)
-        except (ValueError, series.TestSeriesError) as err:
-            output.fprint("Invalid test arguments:", color=output.RED)
-            output.fprint(err)
-            return errno.EINVAL
-
-        # Check filters, append/remove tests.
-        try:
-            tests = self.filter_tests(pav_cfg, args, args.tests)
-        except CommandError as err:
-            output.fprint(err)
-            return errno.EINVAL
+        # Get filtered Test IDs, and then load TestRun objects.
+        test_ids = cmd_utils.arg_filtered_tests(pav_cfg, args)
+        tests = [TestRun.load(pav_cfg, test_id) for test_id in test_ids]
 
         evals = self.build_evaluations_dict(args.x, args.y)
 
@@ -112,7 +80,7 @@ class GraphCommand(commands.Command):
             output.fprint("Evaluations resulted in error:", color=output.RED)
             output.fprint(err)
             return errno.EINVAL
-
+        output.fprint("Plotting...")
         ax = plt.gca()
         for test_id, results in test_results.items():
             color = next(ax._get_lines.prop_cycler)['color']
@@ -126,167 +94,13 @@ class GraphCommand(commands.Command):
         plt.legend()
         plt.show()
 
-    def validate_args(self, args):
-        """Validates all arguments passed to the graph command. Will change
-        :param args: The parsed command line argument object.
-        """
-
-        # Validate Date.
-        if args.date:
-            args.date = args.date.strip("'")
-            args.date = datetime.strptime(args.date, '%b %d %Y')
-
+    def validate_args(self, args) -> None:
         if not args.x:
             raise CommandError("No value was given to graph on X-axis. Use "
                                "--x flag to specify.")
         if not args.y:
             raise CommandError("No values were given to graph on y-axis. "
                                "Use --y flag to specify.")
-
-    def expand_ranges(self, test_list):
-        """Converts a string range of test IDs or series IDs into a list of
-        tests or series strings. Note, this is inclusive. Therefore a range of
-        123-126 will include 126.
-        :param list test_list: A list of tests, series, or ranges strings.
-        :return list update_test_list: A list of test or series strings.
-        """
-
-        updated_test_list = []
-        for test in test_list:
-            if '-' in test:
-                lower, higher = test.split('-')
-                if lower.startswith('s') and higher.startswith('s'):
-                    range_list = range(int(lower.strip('s')),
-                                       int(higher.strip('s'))+1)
-                    updated_test_list.extend(['s'+str(x) for x in range_list])
-                else:
-                    range_list = range(int(lower), int(higher)+1)
-                    updated_test_list.extend([str(x) for x in range_list])
-            else:
-                updated_test_list.append(test)
-
-        return updated_test_list
-
-    def normalize_args_tests(self, pav_cfg, test_list):
-        """Converts test strings into string format used on test_run folder
-        names in the working directory, also expands series into their
-        respective test ids.
-        :param pav_cfg: The pavilion configuration.
-        :param list test_lists: A list of test or series strings.
-        :return list normalized_test_list: A list of tests strings, normalized
-                                           to appear as they do in the
-                                           working_dir.
-        """
-
-        if not test_list:
-            return []
-
-        test_list = self.expand_ranges(test_list)
-
-        normalized_test_list = []
-        for test in test_list:
-            # Normalize test string to appear as it would in test_runs dir.
-            if not test.startswith('s'):
-                normalized_test_list.append(test.zfill(7))
-            # If series, expand and normalize
-            else:
-                for s_test in series.TestSeries.from_id(pav_cfg,
-                                                        test).tests:
-                    normalized_test_list.append(str(s_test).zfill(7))
-
-        return normalized_test_list
-
-    def apply_filters(self, pav_cfg, args, test_path):
-        """Apply the search filters to a given test.
-        :param pav_cfg: The pavilion configuration.
-        :param args: The parsed command line argument object.
-        :param test_path: A test path to check.
-        :return none: If a test is filtered out.
-        :return test: A test object is returned if not filtered out.
-        """
-
-        # Make sure given path is a directory.
-        if not test_path.is_dir():
-            return None
-
-        if test_path.name in args.exclude:
-            return None
-
-        # Filter tests by date.
-        if args.date:
-            test_date = datetime.fromtimestamp(test_path.stat().st_ctime)
-            if test_date.date() != args.date.date():
-                return None
-
-        # Filter tests by user.
-        owner = test_path.owner()
-        if args.user and owner not in args.user:
-            return None
-        if owner in args.exclude:
-            return None
-
-        # Load Test Object, to check Host name and Test Name
-        test = TestRun.load(pav_cfg, int(test_path.name))
-
-        # Ensure test has completed, and successfully run.
-        status = test.status.current()
-        if status.state != STATES.COMPLETE:
-            return None
-
-        # Filter tests by test name.
-        if args.test_name and test.name not in args.test_name:
-            return None
-        if test.name in args.exclude:
-            return None
-
-        # Filter tests by suite name.
-        suite = test.config.get('suite')
-        if args.suite and suite not in args.suite:
-            return None
-        if suite in args.exclude:
-            return None
-
-        # Filter tests by sys name.
-        host = test.config.get('host')
-        if args.sys_name and host not in args.sys_name:
-            return None
-        if host in args.exclude:
-            return None
-
-        return test
-
-    def filter_tests(self, pav_cfg, args, tests):
-        """Filter provided tests, or entire directory, returns a list of tests
-        that match the search critetia.
-        :param pav_cfg: The pavilion configuration.
-        :param args: The parsed command line argument object.
-        :return list test_list: A list of valid test objects.
-        """
-
-        tests_dir = pav_cfg.working_dir / 'test_runs'
-        test_list = []
-        # Filter All Tests.
-        if not tests:
-            for test_path in tests_dir.iterdir():
-                test = self.apply_filters(pav_cfg, args, test_path)
-                if test is None:
-                    continue
-                test_list.append(test)
-
-        # Filter provided list of tests.
-        else:
-            for test_id in tests:
-                test_path = tests_dir / test_id
-                test = self.apply_filters(pav_cfg, args, test_path)
-                if test is None:
-                    continue
-                test_list.append(test)
-
-        if not test_list:
-            raise CommandError("No successful, completed tests "
-                               "matched these filters.")
-
-        return test_list
 
     def build_evaluations_dict(self, x_eval, y_eval):
         """Take the parsed command arguments for --x and --y and build
@@ -341,20 +155,20 @@ class GraphCommand(commands.Command):
         """
 
         # Ensure results are values we can plot.
-        if not isinstance(result,(float, int, list)):
+        if not isinstance(result, (float, int, list)):
             raise ResultError("'{}' evaluation resulted in '{}'. "
-                            "Expected result of float, int, or list."
-                            .format(evals,
-                                    type(result).__name__))
+                              "Expected result of float, int, or list."
+                              .format(evals,
+                                      type(result).__name__))
 
         # Ensure that lists contain values that we can plot. 
         if isinstance(result, list):
             for item in result:
                 if not isinstance(item, (int, float)):
                     raise ResultError("'{}' evaluation resulted in a "
-                                    "list that contains invalid type "
-                                    "'{}'.".format(evals,
-                                                   type(item).__name__))
+                                      "list that contains invalid type "
+                                      "'{}'.".format(evals,
+                                                     type(item).__name__))
             return result
 
         else:
@@ -363,7 +177,7 @@ class GraphCommand(commands.Command):
     def get_evaluation_data(self, results, evals):
         """Get the evaluation data to plot out of results.
         :param dict results: The test results dictionary.
-        :param dict evals: The graph command's evalaution arguments.
+        :param dict evals: The graph command's evaluation arguments.
         :return list x_data_list: The list of x values to plot.
         :return list y_data_list: The list of y values to plot.
         """
@@ -433,9 +247,3 @@ class GraphCommand(commands.Command):
             test_results[test.id] = results
 
         return test_results
-
-class InvalidDateError(RuntimeError):
-    """Error to raise for invalid date argument."""
-
-class ResultError(RuntimeError):
-    """Error to raise when results don't go as expected."""
