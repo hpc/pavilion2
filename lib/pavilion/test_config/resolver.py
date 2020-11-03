d2fa148ac38894a0a332a9d06563a5796d6759ae
Paul Ferrell <51765748+Paul-Ferrell@users.noreply.github.com>
2020-11-03 07:54:57 -0700

    2.2 bugfixes 3 (#333)
    * Initial commit.
    
    * Incorporated some 2.2 hotfixes.
    
    * Fixed issues with deeply deferred variables.
    
    * Fixed several issues.
    
    * added copyfiles fix (and log recurse fix).
    
    * Fixed path issue in log setup.
    
    * Debugging an issue that only shows up on travis.
    
    * Debugging...
    
    * tmp
    
    * Let's try spinning, that's a neat trick!
    
    * Probably fixed it.
    
    * It works!
    
    * Removed debug prints

--- lib/pavilion/test_config/resolver.py
+++ lib/pavilion/test_config/resolver.py
@@ -283,7 +283,8 @@ class TestConfigResolver:
             for ptest_cfg, pvar_man in permuted_tests:
                 # Resolve all variables for the test (that aren't deferred).
                 try:
-                    resolved_config = self.resolve_config(ptest_cfg, pvar_man)
+                    resolved_config = self.resolve_test_vars(
+                        ptest_cfg, pvar_man)
                 except TestConfigError as err:
                     msg = ('In test {} from {}:\n{}'
                            .format(test_cfg['name'], test_cfg['suite_path'],
@@ -740,8 +741,7 @@ class TestConfigResolver:
 
         return suite_tests
 
-    @staticmethod
-    def resolve_permutations(test_cfg, base_var_man):
+    def resolve_permutations(self, test_cfg, base_var_man):
         """Resolve permutations for all used permutation variables, returning a
         variable manager for each permuted version of the test config. We use
         this opportunity to populate the variable manager with most other
@@ -940,7 +940,7 @@ class TestConfigResolver:
         return val.startswith(cls.DEFERRED_PREFIX)
 
     @classmethod
-    def resolve_config(cls, config, var_man):
+    def resolve_test_vars(cls, config, var_man):
         """Recursively resolve the variables in the value strings in the given
         configuration.
 
