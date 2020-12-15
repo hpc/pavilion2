69d2914c897849bfeae780549214bd7efba7919e
Francine Lapid <55203623+francinelapid@users.noreply.github.com>
2020-12-15 14:22:32 -0700

    fixed restart bug (#359)

--- lib/pavilion/series.py
+++ lib/pavilion/series.py
@@ -702,9 +702,10 @@ differentiate it from test ids."""
                 # wait for all the tests to be finished to continue
                 done = False
                 while not done:
+                    self.update_finished_list(finished, started)
                     done = True
                     for set_name, set_obj in self.test_sets.items():
-                        if not set_obj.is_done:
+                        if not set_obj.done:
                             done = False
                             break
                     time.sleep(0.1)
