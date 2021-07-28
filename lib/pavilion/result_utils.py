6769fd12c25ef739f8c066c2f7cb0969ecd0361a
Daniel J Magee <dmagee@lanl.gov>
2021-07-28 14:19:36 -0600

    Parallelize results query.
    Add file result_utils.py to provide an analogue of status_utils.py for the results command.
    The run method from the result.py plugin now calls get_results rather than initializing TestRun.load itself.  Also, get_results returns the test.results dict rather than a TestRun object (which can't be returned by a parallel map invocation because in that case pickle and the TestRun object don't get along.  A result_log field is added to that dict in case show_log is True.  This also simplifies the run command.  The update_results method is not parallel and still requires a full TestRun object so the argument tests is changed to test_ids, a list of ints, which are then initialized in update_results.

---
+++ lib/pavilion/result_utils.py
@@ -0,0 +1,74 @@
+"""A collection of utilities for getting the results of current and past
+test runs and series."""
+
+import os
+import sys
+import time
+import psutil
+import multiprocessing as mp
+
+from functools import partial
+
+from pavilion import status_utils
+from pavilion.test_run import (TestRun, TestRunError, TestRunNotFoundError)
+
+# I suppose these are all the keys of the TestRun.results dict and the essential ones.
+# I'm not sure which to use here or something else, discuss with reviewer.
+
+BASE_FIELDS = [
+    'id',
+    'name',
+    'sys_name',
+    'started',
+    'finished',
+    'result',
+    'results_log'
+]
+
+RESULT_KEYS = [ "id","name","test_version","pav_version","created","started","finished",
+                "duration","user","job_id","permute_on","sched","sys_name","pav_result_errors",
+                "per_file","uuid","return_value","result","results_log"]
+
+
+def get_result(test_id, pav_conf):
+    """Return the result for a single test_id.
+    Add result_log (path) to results dictionary.
+    :param pav_conf: The Pavilion config.
+    :param test_id: The test id being queried.
+    """
+
+    try:
+        test_full = TestRun.load(pav_conf, test_id)
+        test = test_full.results
+        test['results_log'] = test_full.results_log
+
+    except (TestRunError, TestRunNotFoundError) as err:
+        test = {'id': test_id}
+        for k in BASE_FIELDS[1:]:
+            test[k] = None
+
+        test['result'] = "Test not found: {}".format(err)
+
+    return test
+
+
+
+def get_results(pav_cfg, test_ids, errfile=None):
+    """Return the results for all given test id's.
+    :param pav_cfg: The Pavilion config.
+    :param List[str] test_ids: A list of test ids to load.
+    :param errfile: Where to write standard error to.
+    """
+
+    test_ids = status_utils.get_tests(pav_cfg, test_ids, errfile=errfile)
+
+    get_this_result = partial(get_result, pav_conf=pav_cfg)
+
+    if sys.version_info.minor > 6:
+        ncpu = min(psutil.cpu_count(logical=False), len(test_ids))
+        p = mp.Pool(processes=ncpu)
+        tests = p.map(get_this_result, test_ids)
+    else:
+        tests = map(get_this_result, test_ids)
+
+    return tests
