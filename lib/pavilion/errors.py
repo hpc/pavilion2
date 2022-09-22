d4a49f6508508b14643000a01c4d09c33051db7a
Paul Ferrell <pferrell@lanl.gov>
2022-09-22 08:11:08 -0600

    Setup the VariableError class to be properly restored.

--- lib/pavilion/errors.py
+++ lib/pavilion/errors.py
@@ -38,25 +38,34 @@ class VariableError(PavilionError):
     """This error should be thrown when processing variable data,
 and something goes wrong."""
 
-    def __init__(self, message, var_set=None, var=None, index=None, sub_var=None):
+    def __init__(self, message='', var_set=None, var=None, index=None, sub_var=None):
 
+        self.raw_message = message
         self.var_set = var_set
         self.var = var
-        self.index = index
+        if isinstance(index, str) and index.isnumeric():
+            self.index = int(index)
+        else:
+            self.index = index
         self.sub_var = sub_var
 
         key = [str(self.var)]
         if self.var_set is not None:
             key.insert(0, self.var_set)
         if self.index is not None and self.index != 0:
-            key.append(self.index)
+            key.append(str(self.index))
         if self.sub_var is not None:
             key.append(self.sub_var)
 
         key = '.'.join(key)
 
-        message = "Error processing variable key '{}': {}".format(key, message)
+        message = "Error processing variable key '{}': {}".format(key, ''.join(message))
+
         super().__init__(message)
+
+    def __reduce__(self):
+
+        return type(self), (self.raw_message, self.var_set, self.var, self.index, self.sub_var)
 
 
 class DeferredError(VariableError):
