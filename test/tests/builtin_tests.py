7e63ffe6396c162f23ca29329d8d11274334128a
Hank Wikle <hwikle@lanl.gov>
2024-03-01 11:42:18 -0700

    add tests for builtins

---
+++ test/tests/builtin_tests.py
@@ -0,0 +1,11 @@
+"""Test Pavilion builtins."""
+
+import subprocess as sp
+
+from pavilion.unittest import PavTestCase
+
+
+class BuiltinTests(PavTestCase):
+    """Test Pavilion builtins."""
+
+    ...
