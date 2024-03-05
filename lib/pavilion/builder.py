02fba19efae2c87fc34a76d3aa7b89b254df9f85
David DeBonis <ddebonis@lanl.gov>
2024-03-05 12:40:40 -0700

    Revert "fix for issue with source_download set to latest"
    This reverts commit 4c8c3a07b674708430d2bfb6365c8de459e39eb4.

--- lib/pavilion/builder.py
+++ lib/pavilion/builder.py
@@ -319,7 +319,7 @@ class TestBuilder:
 
         if (src_url is not None
                 and ((src_download == 'missing' and found_src_path is None)
-                     or src_download == 'latest' and found_src_path is None)):
+                     or src_download == 'latest')):
 
             if self._download_dest is None:
                 raise TestBuilderError(
