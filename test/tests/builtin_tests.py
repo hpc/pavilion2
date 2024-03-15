1c8e166e9376ea0819ddfb2e9ef34ad429075f19
Hank Wikle <hwikle@lanl.gov>
2024-03-15 09:59:03 -0600

    progress towards survey tests

--- test/tests/builtin_tests.py
+++ test/tests/builtin_tests.py
@@ -1,11 +1,25 @@
-"""Test Pavilion builtins."""
-
-import subprocess as sp
-
+from pavilion import arguments, commands, plugins
+from pavilion.status_file import STATES
 from pavilion.unittest import PavTestCase
 
 
 class BuiltinTests(PavTestCase):
     """Test Pavilion builtins."""
 
-    ...
+    def set_up(self):
+        plugins.initialize_plugins(self.pav_cfg)
+        build_cmd = commands.get_command('build')
+        build_cmd.silence()
+
+    def test_survey(self):
+        arg_parser = arguments.get_parser()
+        args = arg_parser.parse_args([
+            'build',
+            '-H', 'this',
+            '-m',
+            'survey'
+        ])
+
+        build_cmd = commands.get_command(args.command_name)
+        build_ret = build_cmd.run(self.pav_cfg, args)
+        self.assertEqual(build_ret, 0, msg=build_cmd.outfile.read())
