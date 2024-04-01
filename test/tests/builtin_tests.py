27e4f176a9fb4fa2d714eb5dd56111c4a9339f58
Hank Wikle <hwikle@lanl.gov>
2024-04-01 10:28:16 -0600

    add hello_world c program to test survey

--- test/tests/builtin_tests.py
+++ test/tests/builtin_tests.py
@@ -1,29 +1,25 @@
-from pavilion import arguments, commands, plugins
-from pavilion.status_file import STATES
+from pavilion import commands, plugins, arguments
 from pavilion.unittest import PavTestCase
 
 
 class BuiltinTests(PavTestCase):
     """Test Pavilion builtins."""
 
-    def set_up(self):
-        plugins.initialize_plugins(self.pav_cfg)
-        build_cmd = commands.get_command('build')
-        build_cmd.silence()
+    def setUp(self):
+        plugins.initialize_plugins(self.pav_cfg)        
+        run_cmd = commands.get_command("run")
+        run_cmd.silence()
 
-    def test_survey(self):
+    def test_survey_mode_config(self):
         arg_parser = arguments.get_parser()
 
         args = arg_parser.parse_args([
-            'build',
+            'run',
             '-H', 'this',
-            'hello_c'
+            '-m', 'survey',
+            'hello_world_c'
         ])
 
-
-        build_cmd = commands.get_command('build')
-        build_ret = build_cmd.run(self.pav_cfg, args)
-        self.assertEqual(build_ret, 0, msg=build_cmd.outfile.read())
-        # run_cmd = commands.get_command('run')
-        # run_ret = run_cmd.run(self.pav_cfg, args)
-        # self.assertEqual(run_ret, 0, msg=run_cmd.outfile.read())
+        run_cmd = commands.get_command(args.command_name)
+        ret = run_cmd.run(self.pav_cfg, args)
+        self.assertEqual(ret, 0)
