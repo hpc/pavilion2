c7d20be60f9afcaf1489aef82074b1b8098da001
Paul Ferrell <pferrell@lanl.gov>
2024-04-17 07:38:51 -0600

    Just updated the copy_files documentation.

--- lib/pavilion/test_config/file_format.py
+++ lib/pavilion/test_config/file_format.py
@@ -585,10 +585,13 @@ expected to be added to by various plugins.
                     help_text='Commands to run after inherited build '
                               'commands.'),
                 yc.ListElem(
-                    'copy_files', sub_elem=yc.StrElem(),
-                    help_text="When attaching the build to a test run, copy "
-                              "these files instead of creating a symlink. "
-                              "They may include path glob wildcards, "
+                    'copy_files', sub_elem.yc.StrElem(),
+                    help_text="Make these files editable by test runs. "
+                              "Normally files in a test run's build/run directory are "
+                              "symlinks to read-only files in the shared build. "
+                              "Files listed here will be fully copied for each test "
+                              "run instead, and set with user/group write permissions. "
+                              "You may include path glob wildcards, "
                               "including the recursive '**'."),
                 PathCategoryElem(
                     'create_files',
