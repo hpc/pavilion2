7af0fc47e4316598e945980023a227a3a8bb2090
Hank Wikle <hwikle@lanl.gov>
2024-06-20 11:46:14 -0600

    Add validation decorators for transformer methods

---
+++ lib/pavilion/filters/decorators.py
@@ -0,0 +1,83 @@
+"""Defines decorators used by the filter transformer for validating input types
+and determining which functions to dispatch"""
+
+from fnmatch import fnmatch
+from typing import Any, Callable, List
+
+from .filters import FilterParseError
+
+id_func = lambda x, y: x
+all_match = lambda x, y: all(map(lambda z: fnmatch(z, y), x))
+
+def validate_int(func: Callable[[object], Any]) -> Callable[[object, str, str], bool]:
+    """Decorator for methods defined on FilterTransformer whose return values are
+    expected to be compared against integers, used in parsing expressions. The
+    function returned from this decorator takes the comparator and righthand
+    operands (still in the form of strings) as its arguments, checks that the
+    righthand operand can be parsed as an integer, then performs the comparison
+    specified by the comparator string against the output of the decorated function.
+    If the validation fails, or if the comparator string does not specify a valid
+    comparison operator, raises a FilterParseError."""
+    
+    def ifunc(self, comp: str, rval: str) -> bool:
+        try:
+            rval = int(rval)
+        except TypeError:
+            raise FilterParseError(f"Invalid value {rval} for operator {lval}")
+
+        lval = func(self)
+
+        if comp == '=':
+            return lval == rval
+        elif comp == '<':
+            return lval < rval
+        elif comp == '>':
+            return lval > rval
+        elif comp == '<=':
+            return lval <= rval
+        elif comp == '>=':
+            return lval >= rval
+        elif comp == '!=':
+            return lval != rval
+        else:
+            raise FilterParseError(f"Invalid comparator for type int: {comp}")
+
+    return ifunc
+
+def validate_glob(func: Callable[[object], str]) -> Callable[[object, str, str], bool]:
+    """Decorator for methods defined on FilterTransformer whose return values are
+    expected to be compared against globs, used in parsing expressions. The function
+    returned from this decorator takes the comparator and righthand operands (still in
+    the form of strings) as its arguments, then performs the comparison specified by
+    the comparator string. If the validation fails, or if the comparator string does
+    not specify a valid comparison operation, raises a FilterParseError."""
+    
+    def gfunc(self, comp: str, rval: str) -> bool:
+        lval = func(self)
+
+        if comp == '=':
+            return fnmatch(lval, rval)
+        elif comp == '!=':
+            return not fnmatch(lval, rval)
+        else:
+            raise FilterParseError(f"Invalid comparator for type str: {comp}")
+
+    return gfunc
+
+def validate_glob_list(func: Callable[[object], List[str]]) -> Callable[[object, str, str], bool]:
+    """Decorator for methods defined on FilterTransformer whose return values are lists,
+    the elements of which are expected to be compared against globs. The function
+    returned from this decorator takes the comparator and righthand operands (still in
+    the form of strings) as its arguments, then performs the comparison specified by the
+    comparator string. If the validation fails, or if the comparator string does not
+    specify a valid comparison operation, raises a FilterParseError."""
+
+    def glfunc(self, comp: str, rval: str) -> bool:
+        lval = func(self)
+
+        if comp == '=':
+            return all(map(lambda x: fnmatch(x, rval), lval))
+        else:
+            raise FilterParseError(f"Invalid comparator for type str: {comp}")
+
+    return glfunc
