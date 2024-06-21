ec211434710e92887516734aaa6b8769c3ed62a4
Hank Wikle <hwikle@lanl.gov>
2024-06-21 09:52:22 -0600

    Refine and generalize filter validation logic

---
+++ lib/pavilion/filters/validators.py
@@ -0,0 +1,95 @@
+"""Defines decorators used by the filter transformer for validating input types
+and determining which functions to dispatch"""
+
+from fnmatch import fnmatch
+from typing import Callable, List, TypeVar
+
+from .filters import FilterParseError
+
+
+T = TypeVar("T")
+ID = lambda x: x
+
+
+def make_validator(rtype: Callable[[str], T],
+    comp_func: Callable[[T, str, T], bool]) -> Callable[[object, str, str], bool]:
+    """Makes a decorator that validates a comparison expression, ensuring that its
+    righthand operand is of type rtype, produces the lefthand operand by calling
+    the decorated function (intended to be a method of FilterTransform), then
+    compares the lefthand and righthand operands using comp_func, which also
+    takes the expression's operator as an argument."""
+
+    def validate(func: Callable[[object], T]) -> Callable[[object, str, str], bool]:
+
+        def compare(self, comp: str, rval: str) -> bool:
+            try:
+                rval = rtype(rval)
+            except ValueError:
+                raise FilterParseError(f"Invalid value {rval} for type {rtype}.")
+
+            lval = func(self)
+
+            return comp_func(lval, comp, rval)
+
+        return compare
+
+    return validate
+
+
+def comp_int(lval: int, comp: str, rval: int) -> bool:
+    if comp == '=':
+        return lval == rval
+    if comp == '!=':
+        return lval != rval
+    if comp == '<':
+        return lval < rval
+    if comp == '>':
+        return lval > rval
+    if comp == '<=':
+        return lval <= rval
+    if comp == '>=':
+        return lval >= rval
+
+    raise FilterParseError(f"Invalid comparator {comp} for type int.")
+
+
+def comp_glob(lval: str, comp: str, rval: str) -> bool:
+    lval = lval.upper() # fnmatch is case sensitive, despite docs
+    rval = rval.upper()
+
+    if comp == '=':
+        return fnmatch(lval, rval)
+    if comp == '!=':
+        return not fnmatch(lval, rval)
+
+    raise FilterParseError(f"Invalid comparator {comp} for (str, glob).")
+
+
+def comp_glob_list(lval: List[str], comp: str, rval: str) -> bool:
+
+    if comp != '=':
+        raise FilterParseError(f"Invalid comparator {comp} for (List[str], glob).")
+
+    lval = map(lambda x: x.upper(), lval) # fnmatch is case sensitive, despite docs
+    rval = rval.upper()
+
+    matches = map(lambda x: fnmatch(x, rval), lval)
+
+    return all(matches)
+
+
+def comp_str_list(lval: List[str], comp: str, rval: str) -> bool:
+    
+    if comp != '=':
+        raise FilterParseError(f"Invalid comparator {comp} for (List[str], str).")
+
+    lval = map(lambda x: x.upper(), lval)
+    rval = rval.upper()
+
+    return rval in lval
+
+
+validate_int = make_validator(int, comp_int)
+validate_glob = make_validator(ID, comp_glob)
+validate_glob_list = make_validator(ID, comp_glob_list)
+validate_str_list = make_validator(ID, comp_str_list)
