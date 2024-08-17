61e7eac1db7951448a1f2ea0cf9a33e5288cde0d
Hank Wikle <hwikle@lanl.gov>
2024-08-17 12:21:55 -0600

    Improve suites test

--- test/tests/suites_tests.py
+++ test/tests/suites_tests.py
@@ -1,19 +1,27 @@
+from pavilion import arguments
+from pavilion import commands
+from pavilion import plugins
 from pavilion.unittest import PavTestCase
 
 
 class SuitesTests(PavTestCase):
 
-    def test_suite_directory_created(self):
-        config = {
-            'name': 'suite_dir_test',
-            'scheduler': 'raw',
-            'build': {
-                'cmds': ['echo', 'Hello']
-            }
-        }
+    def setUp(self):
+        plugins.initialize_plugins(self.pav_cfg)
+        run_cmd = commands.get_command('run')
+        # run_cmd.silence()
 
-        test = self._quick_test(config, 'suite_dir_test', build=False)
-        test.build()
+    def test_run_from_suite_directory(self):
+        """Test that Pavilion can find and run a test from
+        suites directory."""
 
-        cfg_dir = test.working_dir / 'pav_cfgs'
-        configs = list(cfg_dir.iterdir())
+        arg_parser = arguments.get_parser()
+        args = arg_parser.parse_args([
+            'run',
+            '-H', 'this',
+            'basic_suite_test'
+        ])
+
+        run_cmd = commands.get_command(args.command_name)
+
+        self.assertEqual(run_cmd.run(self.pav_cfg, args), 0)
