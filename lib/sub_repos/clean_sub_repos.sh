aee5697d600384d986a06004fc483d32ab6ea1f9
Paul Ferrell <pferrell@lanl.gov>
2025-04-23 13:10:01 -0600

    More cleanup.

--- lib/sub_repos/clean_sub_repos.sh
+++ lib/sub_repos/clean_sub_repos.sh
@@ -2,15 +2,18 @@
 
 # This command cleans out unnecessary files from subrepos.
 
-not_needed="tests docs examples test .git .github"
+not_needed="tests docs examples test .git .git*"
 
 for dir in $(ls -1); do
     if ! [[ -d $dir ]]; then
         continue
     fi
     for extra_dir in $not_needed; do
-        if [[ -d $dir/$extra_dir ]]; then
-            rm -rf $dir/$extra_dir
-        fi
+        for edir in $dir/$extra_dir; do
+            if [[ -e $edir ]]; then
+                echo "Removing $edir"
+                rm -rf $edir
+            fi
+        done
     done
 done
