94552b6f7afb671d60878992cca56ae74d0ffc06
Hank Wikle <hwikle@lanl.gov>
2025-05-20 16:02:51 -0600

    Revert accidentally committed file

--- lib/pavilion/commands/run.py
+++ lib/pavilion/commands/run.py
@@ -135,9 +135,7 @@ class RunCommand(Command):
         # Note: We have to get a few arguments this way because this code
         # is reused between the build and run commands, and the don't quite have the
         # same arguments.
-        if args.name == "all":
-            ...
-        elif args.name is not None:
+        if args.name:
             series_name = args.name
         else:
             series_name = 'cmdline'
