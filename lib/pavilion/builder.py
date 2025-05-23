80a871da9af826d9fd88130cd4fd96f46e7b6bac
Hank Wikle <hwikle@lanl.gov>
2025-05-23 09:51:48 -0600

    Remove trailing whitespace

--- lib/pavilion/builder.py
+++ lib/pavilion/builder.py
@@ -252,7 +252,7 @@ class TestBuilder:
                 hash_obj.update(self._hash_file(full_path))
             elif full_path.is_dir():
                 self._date_dir(full_path)
-                hash_obj.update(self._hash_dir(full_path, 
+                hash_obj.update(self._hash_dir(full_path,
                     exclude=CONFIG_FNAMES))
             else:
                 raise TestBuilderError(
