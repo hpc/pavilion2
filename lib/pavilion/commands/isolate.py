fbb18dc738a18da284240fad96a5e2705d4b2fbe
Hank Wikle <hwikle@lanl.gov>
2025-12-02 14:48:53 -0700

    Fix missing import

--- lib/pavilion/commands/isolate.py
+++ lib/pavilion/commands/isolate.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser, Namespace, Action
 from pathlib import Path
 import tarfile
 import sys
+import shutil
 from typing import Iterable
 
 from pavilion import output
