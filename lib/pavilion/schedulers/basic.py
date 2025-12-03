43fa7212a9e6f3a79acd48a2e5e12ea1f9bf04e6
Hank Wikle <hwikle@lanl.gov>
2025-12-03 13:34:03 -0700

    Fix rebase

--- lib/pavilion/schedulers/basic.py
+++ lib/pavilion/schedulers/basic.py
@@ -98,20 +98,7 @@ class SchedulerPluginBasic(SchedulerPlugin, ABC):
             for test in test_bin:
                 test.job = job
 
-<<<<<<< HEAD
             script = self.create_kickoff_script(pav_cfg, test_bin, job.kickoff_log)
-=======
-            script = self._create_kickoff_script_stub(
-                pav_cfg=pav_cfg,
-                job_name=job_name,
-                log_path=job.kickoff_log,
-                sched_config=sched_config,
-                node_range=node_range,
-                shebang=test.shebang)
-
-            test_ids = ' '.join(str(test.id) for test in tests)
-            script.command('pav _run {}'.format(test_ids))
->>>>>>> 0e54304d (Fix logging tests)
             script.write(job.kickoff_path)
 
             try:
