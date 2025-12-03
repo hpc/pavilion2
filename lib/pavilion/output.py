e5e1d69818da6db75700eeed2c8da771bb2292d3
Hank Wikle <hwikle@lanl.gov>
2025-12-03 12:55:34 -0700

    Add GroupID to serializer

--- lib/pavilion/output.py
+++ lib/pavilion/output.py
@@ -40,7 +40,7 @@ from pathlib import Path
 from typing import List, Dict, Union, TextIO, Any, Optional, Callable
 
 from pavilion import errors
-from pavilion.test_ids import TestID, SeriesID
+from pavilion.test_ids import TestID, SeriesID, GroupID
 
 BLACK = 30
 RED = 31
@@ -973,7 +973,7 @@ class PavEncoder(json.JSONEncoder):
         # Just auto-convert anything that looks like a dict.
         elif isinstance(o, (dict, UserDict)):
             return dict(o)
-        elif isinstance(o, (TestID, SeriesID)):
+        elif isinstance(o, (TestID, SeriesID, GroupID)):
             return str(o)
         # or has an 'as_dict' method
         elif hasattr(o, 'as_dict'):
