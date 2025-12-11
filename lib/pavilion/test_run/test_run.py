039a2bf9d5df6ad60daf800eeea19e689ffe41c0
Hank Wikle <hwikle@lanl.gov>
2025-12-11 08:50:37 -0700

    Fix loading legacy runs

--- lib/pavilion/test_run/test_run.py
+++ lib/pavilion/test_run/test_run.py
@@ -451,7 +451,7 @@ class TestRun(TestAttributes):
         if test_id.is_relative() and not legacy:
             test_id = resolve_relative_id(pav_cfg, working_dir, test_id)
 
-        path = working_dir / cls.RUN_DIR / test_id.id
+        path = working_dir / cls.RUN_DIR / str(test_id.id)
 
         if not path.is_dir():
             raise TestRunError("Test directory for test id {} does not exist "
