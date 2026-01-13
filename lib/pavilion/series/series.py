10144deb320da45cdb8792fa40a250f238697420
Hank Wikle <hwikle@lanl.gov>
2026-01-13 13:35:13 -0700

    Tweak logs

--- lib/pavilion/series/series.py
+++ lib/pavilion/series/series.py
@@ -525,8 +525,9 @@ class TestSeries:
 
         # Completion will be set when looked for.
 
-    def log_results(self, loggers: List["ResultLogger"] = None) -> None:
-        """Log the results of each test in the series as tests complete."""
+    def log_results(self, loggers: List["ResultLogger"] = None) -> int:
+        """Log the results of each test in the series as tests complete. Returns the total number
+        of tests logged."""
 
         if loggers is None:
             loggers = self.result_loggers
@@ -551,7 +552,7 @@ class TestSeries:
             try:
                 stardo(log, product(loggers, to_log))
                 logged |= to_log
-                output.fprint(self.outfile, f"Logged {len(to_log)} tests ({len(logged)} total).")
+                output.fprint(self.outfile, f"Logged {len(to_log)} test(s) ({len(logged)} total).")
             except TimeoutError:
                 output.fprint(self.outfile, "Timed out waiting on lock for results log.",
                               color=output.RED)
@@ -567,11 +568,13 @@ class TestSeries:
         # Log any remaining tests after series completion
         to_log = set(self.get_completed()) - logged
 
-        output.fprint(self.outfile, f"Found {len(to_log)} completed tests to log.")
+        output.fprint(self.outfile, f"Found {len(to_log)} remaining test(s) to log.")
 
         stardo(log, product(loggers, to_log))
 
-        output.fprint(self.outfile, "Finished logging results.")
+        output.fprint(self.outfile, f"Finished logging results Logged {len(logged)} test(s) total.")
+
+        return len(logged)
 
     def _run_set(self, test_set: TestSet, build_only: bool, rebuild: bool, local_builds_only: bool):
         """Run all requested tests in the given test set."""
