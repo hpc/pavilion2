fae7c4bf6ac435928e9fba6068a14383ae1c1a31
Hank Wikle <hwikle@lanl.gov>
2026-01-13 15:37:54 -0700

    Fix style issues

--- lib/pavilion/series/series.py
+++ lib/pavilion/series/series.py
@@ -574,7 +574,8 @@ class TestSeries:
 
         logged |= to_log
 
-        output.fprint(self.outfile, f"Finished logging results. Logged {len(logged)} test(s) total.")
+        output.fprint(self.outfile, f"Finished logging results. "
+                                     "Logged {len(logged)} test(s) total.")
 
         return len(logged)
 
