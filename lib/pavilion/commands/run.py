b217f3dcd0841cbf46bcdd1f356fd86a71b969fd
Paul Ferrell <pferrell@lanl.gov>
2026-02-25 07:39:34 -0700

    Adding run info file option.

--- lib/pavilion/commands/run.py
+++ lib/pavilion/commands/run.py
@@ -1,7 +1,7 @@
 """The run command resolves tests by their names, builds them, and runs them."""
 
+import datetime
 import errno
-import sys
 from collections import defaultdict
 from pathlib import Path
 
@@ -112,6 +112,11 @@ class RunCommand(Command):
             help="Deprecate existing builds of these tests and rebuild. This "
                  "should be necessary only if the system or user environment "
                  "under which Pavilion runs has changed."
+        )
+        parser.add_argument(
+            '--run-data', default=None, type=Path,
+            help="Write file containing some info on the started test series. This is useful "
+                 "for scripting Pavilion.")
         )
         parser.add_argument(
             'tests', nargs='*', action='store', metavar='TEST_NAME',
@@ -233,3 +238,20 @@ class RunCommand(Command):
                           color=output.RED)
             output.fprint(self.errfile, err.pformat())
             return errno.EINVAL
+
+    def _write_run_data(self, out_path: Path, series_obj: Series):
+        """Output info about the series. Print to stdout by default."""
+
+        outfile = self.outfile
+        if out_path is not None:
+            try:
+                outfile = out_path.open('w')
+            except (OSError, PermissionError) as err:
+                output.fprint(self.errfile, f"Warning: Could not open specified file for run data '{out_path}': {err}")
+
+        started = datetime.datetime.now().isoformat(' ')
+        output.fprint(outfile, '-------------------------')
+        output.fprint(outfile, f' sid: {series_obj.id}')
+        output.fprint(outfile, f' tests: {len(series_obj.tests)}')
+        output.fprint(outfile, f' started: {started}')
+        output.fprint(outfile, '-------------------------')
