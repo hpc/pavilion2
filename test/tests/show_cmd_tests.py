c9192632fdcbebdaf85e4e7e49b6ed6af63ad1c6
Hank Wikle <hwikle@lanl.gov>
2026-04-02 15:10:03 -0600

    Add comprehensive unit tests for `pav show collections`

--- test/tests/show_cmd_tests.py
+++ test/tests/show_cmd_tests.py
@@ -91,6 +91,45 @@ class ShowTests(unittest.PavTestCase):
 
         self.assertIsInstance(data, list, f"Expected JSON list.\nReceived:\n{data}")
 
+     def test_collections_subcommand(self):
+        """Test that the collections subcommand, with no arguments, works as expected."""
+
+        parser = arguments.get_parser()
+
+        show_cmd = commands.get_command('show')
+        show_cmd.silence()
+
+        args = parser.parse_args(("show", "collections"))
+
+        self.assertEqual(show_cmd.run(self.pav_cfg, args), 0,
+                         msg='pav show collections terminated with non-zero error code.')
+
+        output = show_cmd.outfile.getvalue()
+
+        self.assertNotEqual(output, "", "pav show collections gave empty output")
+
+    def test_collections_subcommand_format_argument(self):
+        """Test that the collections subcommand --format argument behaves as expected."""
+
+        parser = arguments.get_parser()
+
+        show_cmd = commands.get_command('show')
+        show_cmd.silence()
+
+        args = parser.parse_args(("show", "collections", "--format", "json"))
+
+        self.assertEqual(show_cmd.run(self.pav_cfg, args), 0,
+                         msg='pav show collections --format json terminated with non-zero error code.')
+
+        output = show_cmd.outfile.getvalue()
+
+        try:
+            data = json.loads(output)
+        except Exception as e:
+            self.fail(f"pav show collections --format json did not produce valid JSON. Output\n{output}")
+
+        self.assertIsInstance(data, list, f"Expected JSON list.\nReceived:\n{data}")
+
     def test_functions_subcommand(self):
         """Test that the functions subcommand, with no arguments, works as expected."""
 
