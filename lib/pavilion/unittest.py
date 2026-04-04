ba4b82708d8d51442798897603232236a2d162af
Hank Wikle <hwikle@lanl.gov>
2026-04-04 11:38:34 -0600

    Fix string format error

--- lib/pavilion/unittest.py
+++ lib/pavilion/unittest.py
@@ -452,7 +452,7 @@ The default config is: ::
                 continue
         else:
             raise TimeoutError(
-                f"Timed out out after {} seconds. Waiting on tests: {}"
+                "Timed out out after {} seconds. Waiting on tests: {}"
                 .format(timeout, [test.name for test in dir_db.select(self.pav_cfg,
                                                             runs_dir).paths
                         if is_complete(test)]))
