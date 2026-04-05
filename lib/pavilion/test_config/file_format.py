47cbc679b2c72182cf2c0b4c7c08c882a14939b7
Hank Wikle <hwikle@lanl.gov>
2026-04-05 17:04:24 -0600

    Fix inconsistent shebang defaults

--- lib/pavilion/test_config/file_format.py
+++ lib/pavilion/test_config/file_format.py
@@ -489,7 +489,7 @@ expected to be added to by various plugins.
             help_text="Set by pavilion. An id to identify the base config shared by "
                       "a set of permutations."),
         yc.StrElem(
-            'shebang', default='#!/usr/bin/bash',
+            'shebang', default='#!/bin/bash',
             help_text="The shebang to put at the top of build/run/kickoff scripts. "
                       "Should always point to 'bash', but the path and options may vary "
                       "per-system."),
