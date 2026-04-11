c92717814f76ebac9161783cbe4c70260697c237
Hank Wikle <hwikle@lanl.gov>
2026-04-11 13:46:16 -0600

    Fix syntax error

--- lib/pavilion/commands/make_activate.py
+++ lib/pavilion/commands/make_activate.py
@@ -32,13 +32,13 @@ class MakeActivateCommand(Command):
                             help="Config directory location. If none is provided, the script "
                                  "derives the value from the directory in which it is run.",
                             type=Path)
-        parser.add_argument("-p", "--pav-src", type=Path, default=self.this_pav_src
+        parser.add_argument("-p", "--pav-src", type=Path, default=self.this_pav_src,
                             help="Path to the Pavilion source directory. If none is provided, "
                                  "defaults to a subdirectory of directory in which the script is "
                                  "run, with the same name as the root directory of the current "
                                  f"Pavilion repository (currently {self.this_pav_src}).")
         parser.add_argument("-f", "--force",
-                            help="Forcibly overwrite the file, if a file with that name "
+                            help="Forcibly overwrite the file, if a file with the same name "
                                  "already exists.")
 
     def run(self, pav_cfg: PavConfig, args: Namespace) -> int:
