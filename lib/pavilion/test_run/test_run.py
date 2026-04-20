48b16abd9a9a93ac0637084be9a6ea778550cfdf
Hank Wikle <hwikle@lanl.gov>
2026-04-20 16:08:37 -0600

    Fix TestRun.load() for older tests

--- lib/pavilion/test_run/test_run.py
+++ lib/pavilion/test_run/test_run.py
@@ -430,15 +430,31 @@ class TestRun(TestAttributes):
         :rtype: TestRun
         """
 
+        series_dir = pav_cfg.working_dir / cls.SERIES_DIR / str(test_id.series.as_int())
+        series_test_runs_dir = series_dir / cls.RUN_DIR
+        series_test_sets_dir = series_dir / "test_sets"
+
         if test_id.is_relative():
+            path = pav_cfg.working_dir / cls.RUN_DIR / str(test_id)
+        elif series_test_runs_dir.exists():
             # Use the series directory's symlink to the test, so we don't have to worry about which
             # config directory it's in
-            path = (pav_cfg.working_dir / cls.SERIES_DIR / str(test_id.series.as_int()) /
-                    cls.RUN_DIR / str(test_id))
+            path = (series_test_runs_dir / str(test_id))
         else:
-            path = pav_cfg.working_dir / cls.RUN_DIR / str(test_id)
+            # For older tests that don't have that symlink
 
-        if not path.is_dir():
+            path = None
+
+            for test_set in series_test_sets_dir.iterdir():
+                if not test_set.is_dir():
+                    continue
+
+                for test_dir in test_set.iterdir():
+                    if test_dir.name == str(test_id):
+                        path = test_dir
+                        break
+
+        if path is None or not path.is_dir():
             raise TestRunError("Test directory for test id {} does not exist "
                                "at '{}' as expected."
                                .format(test_id, path))
